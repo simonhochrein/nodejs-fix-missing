0f4c8e86dc342102a67059ff2259f15702a0f392
ministrycrm-admin <info@ministrycrm.com>
2016-05-17 15:26:07 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# fix-missing
+Fix Missing Dependencies Of Your NodeJS App
+
+This package scans for all ```require()``` statements in your file.
+If the package specified is not installed, (You guessed it) It will install it for you.
+
+## Installation
+```npm i -g fix-missing```
+
+## Usage
+```fix-missing ./YOUR_FILE```
+
+It will also add the installed package to the dependencies.
---
+++ index.js
@@ -0,0 +1,40 @@
+module.exports = function(){
+	if(!process.argv[2]){
+		console.error("Path Required!");
+		return;
+	}
+	var fs = require("fs");
+	var resolve = require("resolve");
+	var currentPath = process.cwd();
+	var currentFile = resolve.sync("./"+process.argv[2], { basedir: process.cwd() });;
+	var file = fs.readFileSync(currentFile,'utf-8');
+	var a = file.match(/require\(.*\)/g);
+	a.forEach(function(val){
+		val = val.replace(/require\([`"'](.*)[`"']\)/,"$1");
+		if(val.indexOf("./")==-1){
+			try{
+				resolve.sync(val,{basedir: process.cwd()});
+			}
+			catch(e){
+				var exec = require('child_process').exec,
+				    child;
+				  console.log("Installing Missing Module '"+val+"'")
+				 child = exec('npm install '+val,
+				 function (error, stdout, stderr) {
+				     console.log(stdout);
+				     console.log("\033[91m"+stderr+"\033[0m");
+				     if (error !== null) {
+				          console.log('exec error: ' + error);
+				     }
+				 });
+			}
+		}
+	});
+	process.on('exit', function(code) {
+	  if(code == 0){
+	  	console.log("\033[94m Analysis Complete!\033[0m");
+	  } else {
+	  	console.log("Exiting with code "+code);
+	  }
+	});
+}
